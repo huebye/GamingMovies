6776881b06f551b10153d5f45172b178287cede8
huebye <thomashuebner94@gmx.de>
2021-04-20 18:03:20 +0200

    designed and implemented new function to index.js
    add function added
    delete function added
    get function added
    designed documentation.html/index.html

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 
 app.use(express.static('public'));
 app.use(morgan('common'));
+app.use(bodyParser.json());
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something must have gone wrong!');
@@ -12,43 +14,53 @@ app.use((err, req, res, next) => {
 let topMovies = [
   {
     title: 'Harry Potter and the Sorcerer\'s Stone',
-    author: 'J.K. Rowling'
+    director: 'J.K. Rowling',
+    genre: 'Fantasy'
   },
   {
     title: 'Focus',
-    author: 'John Requa'
+    director: 'John Requa',
+    genre: 'Action'
   },
   {
     title: 'Twilight',
-    author: 'Stephanie Meyer'
+    director: 'Stephanie Meyer',
+    genre: 'Fantasy'
   },
   {
     title: 'The Lord of the Rings: The Fellowship of the Ring',
-    author: 'J.R.R. Tolkien'
+    director: 'Peter Jackson',
+    genre: 'Fantasy'
   },
   {
-    title: 'The Lord of the Rings: The Two Towers ',
-    author: 'J.R.R. Tolkien'
+    title: 'The Lord of the Rings: The Two Towers',
+    director: 'Peter Jackson',
+    genre: 'Fantasy'
   },
   {
     title: 'The Lord of the Rings: The Return of the King',
-    author: 'J.R.R. Tolkien'
+    director: 'Peter Jackson',
+    genre: 'Fantasy'
   },
   {
     title: 'The Hobbit: An Unexpected Journey',
-    author: 'J.R.R. Tolkien'
+    director: 'Peter Jackson',
+    genre: 'Fantasy'
   },
   {
     title: 'The Hobbit: The Desolation of Smaug ',
-    author: 'J.R.R. Tolkien'
+    director: 'Peter Jackson',
+    genre: 'Fantasy'
   },
   {
     title: 'The Hobbit: The Battle of the Five Armies',
-    author: 'J.R.R. Tolkien'
+    director: 'Peter Jackson',
+    genre: 'Fantasy'
   },
   {
     title: 'Tenet',
-    author: 'Christopher Nolan'
+    director: 'Christopher Nolan',
+    genre: 'Thriller'
   }
 ];
 
@@ -66,7 +78,56 @@ app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
 
+// Get the data about a movie, by title.
 
+app.get('/movies/:title', (req, res) => {
+  res.json(topMovies.find((movie) =>
+    { return movie.title === req.params.title }));
+});
+
+// Get the data about the director of a movie, by name.
+
+app.get('/movies/:director', (req, res) => {
+  res.json(topMovies.find((movie) =>
+    { return movie.director === req.params.director }));
+});
+
+// Get the data about the genre of a movie, by name.
+
+app.get('/movies/:genre', (req, res) => {
+  res.json(topMovies.find((movie) =>
+    { return movie.genre === req.params.genre }));
+});
+
+// Add movie to the list, by name.
+
+app.post('/movies', (req, res) => {
+  let newMovie = req.body;
+
+  if (!newMovie.title) {
+    const message = 'Missing title in request body';
+    res.status(400).send(message);
+  } else {
+    topMovies.push(newMovie);
+    res.status(201).send('The Movie ' + req.body.title + ' was added succesfully!');
+  }
+});
+
+// Deletes movie from the list of movies, by title.
+
+app.delete('/movies/:title', (req, res) =>{
+  let movie = topMovies.find((movie) => {
+    return movie.title === req.params.title
+  });
+  if (movie) {
+    topMovies = topMovies.filter((obj) =>{
+      return obj.title !== req.params.title
+    });
+    res.status(201).send('Movie ' + req.params.title + ' was deleted.');
+  }
+});
+
+const port = process.env.PORT || 8080;
 // listen for requests
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
